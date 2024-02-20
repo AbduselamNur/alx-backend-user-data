3d3070aeb787493ede38b984a5bf2f085dc9b48c
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2024-02-19 23:44:37 -0800

    Change return type of hash_password function to bytes

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,7 +5,7 @@ This module is used to encrypt the password
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """
     Hash the password
     Using: bcrypt package to perform the hashing
