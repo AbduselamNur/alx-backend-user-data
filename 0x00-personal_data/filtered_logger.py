413257b4241133cf049d3972510a0afc5c97ab9b
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2024-02-19 22:02:35 -0800

    Add filtered_logger.py module for obfuscating log messages

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module contains a method called filter_datum that returns the log message
+obfuscated
+"""
+from typing import List
+import re
+import logging
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Returns the log message obfuscated
+    """
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
