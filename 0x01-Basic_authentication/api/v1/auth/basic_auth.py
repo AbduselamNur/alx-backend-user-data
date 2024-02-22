956ab6e8a24de5e6a1f94ba23209e2d15a603407
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2024-02-22 10:28:56 -0800

    Refactor user_object_from_credentials method in BasicAuth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -48,21 +48,22 @@ class BasicAuth(Auth):
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(':', 1))
 
-    def user_object_from_credentials(
-            self, user_email: str, user_pwd: str) -> TypeVar('User'):
-        """Returns the User instance based on his email and password
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
         """
-        if user_email is None or \
-            type(user_email) is not str or \
-            user_pwd is None or \
-                type(user_pwd) is not str:
+        Return a User instance based on email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
             return None
         try:
-            user = User.search({'email': user_email})
-            if user is None:
+            users = User.search({"email": user_email})
+            if not users or users == []:
                 return None
-            if user.is_valid_password(user_pwd):
-                return user
+            for u in users:
+                if u.is_valid_password(user_pwd):
+                    return u
             return None
         except Exception:
             return None
