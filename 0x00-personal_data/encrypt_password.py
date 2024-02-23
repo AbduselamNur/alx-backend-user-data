fb64949729ed74578c91a594adbec2cb186586a9
root <root@DESKTOP-LMQKSJ4.localdomain>
2024-02-23 10:01:33 -0800

    fix

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
-"""
-This module is used to encrypt the password
+"""A module for encrypting passwords.
 """
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
+    """Hashes a password using a random salt.
     """
-    Hash the password
-    Using: bcrypt package to perform the hashing
-    """
-    return bcrypt.hashpw(password.encode(),
-                         bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks is a hashed password was formed from the given password.
     """
-    Check if the password is valid
-    Using: bcrypt package to perform the hashing
-    """
-    return bcrypt.checkpw(password.encode(), hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
